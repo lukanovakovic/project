fcb8f18fa26133c9e1650c6d3452183fd016a395
pc <pc@DESKTOP-ELLA4S6>
2020-07-04 22:22:49 +0200

    Sklonjeno ime

--- project/src/classChapter/Sisa.java
+++
@@ -1,5 +0,0 @@
-package classChapter;
-
-public class Sisa {
-
-}
--- project/src/classChapter/z1televizor/Televizor.java
+++ project/src/classChapter/z1televizor/Televizor.java
@@ -5,6 +5,5 @@ public class Televizor {
 	private int jacinaTona = 0;
 	private int trenutniProgram = 1;
 	private boolean ukljucen = false;
-	private String ime;
 
 }

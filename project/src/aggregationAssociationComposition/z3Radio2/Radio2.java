9268da85375911884360f11a6ef67e90872170a0
pc <pc@DESKTOP-ELLA4S6>
2020-07-13 20:24:41 +0200

    radio2

---
+++ project/src/aggregationAssociationComposition/z3Radio2/Radio2.java
@@ -0,0 +1,37 @@
+package aggregationAssociationComposition.z3Radio2;
+
+public class Radio2 {
+
+	private double fmFrekvencija = 87.5;
+	private int amFrekvencija = 567;
+	private char band = 'F';
+
+	public void podesiFmFrekvenciju(double novaFM) {
+		if (novaFM > 87.5 && novaFM < 108.0)
+			fmFrekvencija = novaFM;
+		else
+			System.out.println("mora biti izmedju 87.5 i 108.0");
+	}
+
+	public void podesiAmFrekvenciju(int novaAM) {
+		if (novaAM > 567 && novaAM < 1576)
+			amFrekvencija = novaAM;
+		else
+			System.out.println("mora biti izmedju 567 i 1576");
+	}
+
+	public void podesiBand(char noviBand) {
+		if (noviBand == 'A')
+			band = 'A';
+		else if (noviBand == 'F')
+			band = 'F';
+		else
+			System.out.println("mora da bude A ili F");
+	}
+
+	public void ispis() {
+		System.out.println(
+				"band = " + band + " amFrekvencija je =  " + amFrekvencija + " fmFrekvencija " + fmFrekvencija);
+	}
+
+}
---
+++ project/src/aggregationAssociationComposition/z3Radio2/Radio2Test.java
@@ -0,0 +1,14 @@
+package aggregationAssociationComposition.z3Radio2;
+
+public class Radio2Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Radio2 radio = new Radio2();
+		radio.podesiFmFrekvenciju(105.0);
+		radio.podesiAmFrekvenciju(1900);
+		radio.podesiBand('F');
+		radio.ispis();
+	}
+
+}

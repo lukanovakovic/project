0971b1b1c9a57a78bb671c9d0a32869c0b0542ac
pc <pc@DESKTOP-ELLA4S6>
2020-07-13 20:28:05 +0200

    osoba

---
+++ project/src/aggregationAssociationComposition/z1FormulaTim/Osoba.java
@@ -0,0 +1,35 @@
+package aggregationAssociationComposition.z1FormulaTim;
+
+public class Osoba {
+
+	private String ime;
+	private String prezime;
+
+	public Osoba(String ime, String prezime) {
+
+		this.ime = ime;
+		this.prezime = prezime;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public void ispis() {
+
+		System.out.println("ime = " + ime + " prezime = " + prezime);
+	}
+
+}

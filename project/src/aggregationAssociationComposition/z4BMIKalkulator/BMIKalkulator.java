1d189d995bcfa5227b1ca456ab094645e79bd3a4
pc <pc@DESKTOP-ELLA4S6>
2020-07-13 20:15:47 +0200

    zadatak BMIKalkulator

---
+++ project/src/aggregationAssociationComposition/z4BMIKalkulator/BMIKalkulator.java
@@ -0,0 +1,36 @@
+package aggregationAssociationComposition.z4BMIKalkulator;
+
+public class BMIKalkulator {
+
+	private String kategorija;
+	private double BMI;
+
+	public double izracunajBMI(double visina, double tezina) {
+		if (tezina > 30 && tezina < 200 && visina < 2.4 && visina > 1.2) {
+			if (tezina / (visina * visina) <= 15) {
+				kategorija = "anoreksija";
+			} else if (tezina / (visina * visina) > 15 && tezina / (visina * visina) < 18.5) {
+				kategorija = "mrsavost";
+			} else if (tezina / (visina * visina) > 18.5 && tezina / (visina * visina) < 25) {
+				kategorija = "normalno";
+			} else if (tezina / (visina * visina) > 25 && tezina / (visina * visina) < 30) {
+				kategorija = "popunjen";
+			} else if (tezina / (visina * visina) > 30 && tezina / (visina * visina) < 40) {
+				kategorija = "debeo";
+			} else if (tezina / (visina * visina) < 40) {
+				kategorija = "gojazan";
+			}
+			BMI = tezina / (visina * visina);
+			return BMI;
+		} else {
+			System.out.println("nije u granicama");
+			return 0;
+
+		}
+	}
+
+	public void ispis() {
+		System.out.println("kategorija = " + kategorija + " BMI  =  " + BMI);
+
+	}
+}
---
+++ project/src/aggregationAssociationComposition/z4BMIKalkulator/BMIKalkulatorTest.java
@@ -0,0 +1,12 @@
+package aggregationAssociationComposition.z4BMIKalkulator;
+
+public class BMIKalkulatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BMIKalkulator bmi = new BMIKalkulator();
+		bmi.izracunajBMI(1.9, 90);
+		bmi.ispis();
+	}
+
+}

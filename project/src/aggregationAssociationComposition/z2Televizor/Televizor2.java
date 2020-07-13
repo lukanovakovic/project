73674298a0036cd4e138c19699b9911e651a4c1b
pc <pc@DESKTOP-ELLA4S6>
2020-07-13 20:25:09 +0200

    televizor2

---
+++ project/src/aggregationAssociationComposition/z2Televizor/Televizor2.java
@@ -0,0 +1,58 @@
+package aggregationAssociationComposition.z2Televizor;
+
+public class Televizor2 {
+
+	private int jacinaTona = 0;
+	private int trenutniProgram = 1;
+	private boolean ukljucen = false;
+
+	public void ukljuci() {
+		if (ukljucen == false)
+			ukljucen = true;
+		else
+			System.out.print("televizor je vec ukljucen");
+	}
+
+	public void iskljuci() {
+		if (ukljucen == true)
+			ukljucen = false;
+		else
+			System.out.print("televizor je vec iskljucen");
+	}
+
+	public void pojacajTon() {
+		if (jacinaTona < 10)
+			jacinaTona++;
+		else
+			System.out.print("jacina tona je na maksimumu");
+
+	}
+
+	public void smanjiTon() {
+		if (jacinaTona > 0)
+			jacinaTona--;
+		else
+			System.out.print("jacina tona je na minimumu");
+
+	}
+
+	public void podesiProgramNavise() {
+		if (trenutniProgram < 99)
+			trenutniProgram++;
+		else
+			trenutniProgram = 1;
+	}
+
+	public void podesiProgramNanize() {
+		if (trenutniProgram < 1)
+			trenutniProgram--;
+		else
+			trenutniProgram = 99;
+	}
+
+	public void ispis() {
+
+		System.out.println("jacina tona je = " + jacinaTona + " trenutni program je = " + trenutniProgram
+				+ " televizor je = " + ukljucen);
+	}
+}
---
+++ project/src/aggregationAssociationComposition/z2Televizor/Televizor2test.java
@@ -0,0 +1,16 @@
+package aggregationAssociationComposition.z2Televizor;
+
+public class Televizor2test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Televizor2 tele = new Televizor2();
+		tele.pojacajTon();
+		tele.ukljuci();
+		tele.ispis();
+		tele.podesiProgramNanize();
+		tele.ispis();
+	}
+
+}

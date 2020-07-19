a89b95efd8070ddf1c017274613b39bda728c43a
pc <pc@DESKTOP-ELLA4S6>
2020-07-19 14:43:37 +0200

    to bi trebalo da je to ispravio sam proveru onu i ispisuje sad valjda sve sto treba

--- project/src/methods/z8automobil/TroskoviRegitstacije.java
+++ project/src/methods/z8automobil/TroskoviRegitstacije.java
@@ -4,48 +4,47 @@ public class TroskoviRegitstacije {
 
 	static double obracunajDoprinos(Automobil auto) {
 
-		if (auto.getKubikaza() > 400 && auto.getKubikaza() < 10000) {
-			if (auto.getKubikaza() < 1000)
-				return 145.5;
-			else if (auto.getKubikaza() >= 1000 && auto.getKubikaza() < 1400)
-				return 200.0;
-			else if (auto.getKubikaza() >= 1400 && auto.getKubikaza() < 2500)
-				return 250.0;
-			else {
-				return 413.3;
-			}
-
-		} else {
-			System.out.println("Pokusaj novog brancha");
+		if (auto.getKubikaza() < 400 && auto.getKubikaza() > 10000) {
 			System.out.println("kubikaza nije u granicama dozvoljenog");
 			return 0;
 		}
+
+		if (auto.getKubikaza() < 1000)
+			return 145.5;
+		else if (auto.getKubikaza() >= 1000 && auto.getKubikaza() < 1400)
+			return 200.0;
+		else if (auto.getKubikaza() >= 1400 && auto.getKubikaza() < 2500)
+			return 250.0;
+		else {
+			return 413.3;
+		}
+
 	}
 
 	static double obracunajOsiguranje(Automobil auto) {
 
-		if (auto.getSnaga() > 20 && auto.getSnaga() < 1000) {
-			if (auto.getSnaga() < 55)
-				return 3000.0;
-			else if (auto.getSnaga() >= 55 && auto.getSnaga() < 75)
-				return 4500.0;
-			else if (auto.getSnaga() >= 75 && auto.getSnaga() < 150)
-				return 7300.0;
-			else {
-				return 9000.0;
-			}
-
-		} else {
+		if (auto.getSnaga() < 20 || auto.getSnaga() > 1000) {
 			System.out.println("snaga nije u granicama dozvoljenog");
 			return 0;
 		}
+
+		if (auto.getSnaga() < 55)
+			return 3000.0;
+		else if (auto.getSnaga() >= 55 && auto.getSnaga() < 75)
+			return 4500.0;
+		else if (auto.getSnaga() >= 75 && auto.getSnaga() < 150)
+			return 7300.0;
+		else
+			return 9000.0;
 
 	}
 
 	static void ispisiSveTroskove(Automobil auto) {
 		double cenaOsiguranja = obracunajOsiguranje(auto);
 		double cenaDoprinosa = obracunajDoprinos(auto);
-		System.out.println("cena osiguranja je " + cenaOsiguranja + " cena doprinosa je " + cenaDoprinosa);
+		double ukupniTroskovi = cenaDoprinosa + cenaOsiguranja;
+		System.out.println("cena osiguranja je " + cenaOsiguranja + " cena doprinosa je " + cenaDoprinosa
+				+ " ukupna cena troskova je = " + ukupniTroskovi);
 
 	}
 }
